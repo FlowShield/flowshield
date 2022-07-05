6b55dd3e134f875d24500bcce0fc8ff868ecab7f
Oct <qianggeek@gmail.com>
2022-07-05 17:31:22 +0000

    feat(contract):Imporve the ERC20

--- contract/contracts/Token.sol
+++ contract/contracts/Token.sol
@@ -7,77 +7,99 @@ contract Token {
     // Some string type variables to identify the token.
     string public name = "My Hardhat Token";
     string public symbol = "MBT";
+    uint8 public decimals = 2;
 
-    // 固定发行量
+
     uint256 public totalSupply = 1000000;
 
-    // An address type variable is used to store ethereum accounts.
-    address public owner;
-
-    //Stake amount
-    uint256 public stakeAmount = 1000;
-
     // A mapping is a key/value map. Here we store each account balance.
-    mapping(address => uint256) balances;
+    mapping(address => uint256) private balances;
+    mapping(address => mapping(address=>uint256)) private allowances;
 
-    // A mapping is a key/value map. Here we store each staked user.
-    mapping(address => uint256) stakeList;
-    /**
-     * 合约构造函数
-     *
-     * The `constructor` is executed only once when the contract is created.
-     * The `public` modifier makes a function callable from outside the contract.
-     */
+    event Transfer(
+        address indexed from,
+        address indexed to,
+        uint256 value
+    );
+    event Approval(
+        address indexed owner,
+        address indexed spender,
+        uint256 value
+    );
     constructor() {
         // The totalSupply is assigned to transaction sender, which is the account
         // that is deploying the contract.
         balances[msg.sender] = totalSupply;
-        owner = msg.sender;
     }
 
-    /**
-     * 代币转账.
-     *
-     * The `external` modifier makes a function *only* callable from outside
-     * the contract.
-     */
-    function transfer(address to, uint256 amount) external {
-        // Check if the transaction sender has enough tokens.
-        // If `require`'s first argument evaluates to `false` then the
-        // transaction will revert.
-        require(balances[msg.sender] >= amount, "Not enough tokens");
-
-        // Transfer the amount.
-        balances[msg.sender] -= amount;
-        balances[to] += amount;
-    }
-
-    /**
-     * 读取某账号的代币余额
-     *
-     * The `view` modifier indicates that it doesn't modify the contract's
-     * state, which allows us to call it without executing a transaction.
-     */
-    function balanceOf(address account) external view returns (uint256) {
+    function balanceOf(address account) public view returns (uint256) {
         return balances[account];
     }
 
     /**
-     * 是否已经质押
-     */
-    function isStake() external view returns (bool) {
-        return stakeList[msg.sender] != 0;
+    * @dev Transfer token for a specified address
+    * @param to The address to transfer to.
+    * @param value The amount to be transferred.
+    */
+    function transfer(address to, uint256 value) public returns (bool) {
+        require(balances[msg.sender] >= value);
+
+        balances[msg.sender] -= value;
+        balances[to] += value;
+        emit Transfer(msg.sender, to, value);
+        return true;
     }
-    
     /**
-     * 质押
-     */
-    function stake() external {
-        require(balances[msg.sender] >= stakeAmount, "Not enough tokens");
-        
-        balances[msg.sender] -= stakeAmount;
-        balances[owner] += stakeAmount;
-        stakeList[msg.sender] = stakeAmount;
+    * @dev Transfer tokens from one address to another
+    * @param from address The address which you want to send tokens from
+    * @param to address The address which you want to transfer to
+    * @param value uint256 the amount of tokens to be transferred
+    */
+    function transferFrom(address from,address to,uint256 value)public returns (bool){
+        require(balances[from] >= value );
+        require(allowances[from][msg.sender] >= value);
+
+        balances[from] -= value;
+        allowances[from][msg.sender] -= value;
+        balances[to] += value;
+        emit Transfer(from, to, value);
+        return true;
     }
+    function approve(address spender, uint256 value)public returns (bool){
+        allowances[msg.sender][spender] = value;
+        emit Approval(msg.sender, spender, value);
+        return true;
+    }
+
+    function allowance(address owner, address spender)public view returns (uint256){
+        return allowances[owner][spender];
+    }
+   
+
+    //Stake amount
+    //uint256 public stakeAmount = 1000;
+
+    
+
+    // // A mapping is a key/value map. Here we store each staked user.
+    // mapping(address => uint256) stakeList;
+
+    // /**
+    //  * 是否已经质押
+    //  */
+    // function isStake() external view returns (bool) {
+    //     return stakeList[msg.sender] != 0;
+    // }
+    
+    // /**
+    //  * 质押
+    //  */
+    // function stake() external {
+    //     require(balances[msg.sender] >= stakeAmount, "Not enough tokens");
+        
+    //     balances[msg.sender] -= stakeAmount;
+    //     balances[owner] += stakeAmount;
+    //     stakeList[msg.sender] = stakeAmount;
+    // }
     
 }
